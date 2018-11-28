d7ff4d4ba1cbb8156b6a06be4ac0372e99db7992
valente3 <valente3@uni.coventry.ac.uk>
2018-11-28 17:20:38 +0000

    chanches

--- sleepCycles.py
+++ sleepCycles.py
@@ -1,10 +1,5 @@
-#from time import gmtime, strftime 
-#import time
-from datetime import date
-from datetime import time
-from datetime import datetime
-from datetime import timedelta
-
+from time import gmtime, strftime 
+import time
 
 """calculates de time you should wake up given a bed time hour"""
 #good night sleep is between 5/6 cycles
@@ -13,52 +8,49 @@ from datetime import timedelta
 def sleepW():
    
  #https://docs.python.org/3/library/time.html
- ##t = strftime("%H:%M", gmtime())
- t1 = datetime.now()
- t2 = t1.strftime("%H:%M")
+ t = strftime("%H:%M", gmtime())
 
- hours, minutes = t2.split(":") 
 
+ hours, minutes = t.split(":") 
  H = int (hours) 
  M = int (minutes)
  cycle = 90  
 
- #it takes an average of 15 min to fall asleep
+
+ # it takes an average of 15 min to fall asleep
 
  minTimeC3 = ( 15 + ((H * 60) + M) + (3 * cycle)) #4.5h of sleep
+ secTimeC3 = 60 * minTimeC3
+
  minTimeC4 = ( 15 + ((H * 60) + M) + (4 * cycle)) #6h of sleep
+ secTimeC4 = 60 * minTimeC4
+
  minTimeC5 = ( 15 + ((H * 60) + M) + (5 * cycle)) #7.5h of sleep
+ secTimeC5 = 60 * minTimeC5
+
  minTimeC6 = ( 15 + ((H * 60) + M) + (6 * cycle)) #9h of sleep
+ secTimeC6 = 60 * minTimeC6
+
+ W3C = time.strftime("%H:%M", time.gmtime(secTimeC3))
+ W4C = time.strftime("%H:%M", time.gmtime(secTimeC4))
+ W5C = time.strftime("%H:%M", time.gmtime(secTimeC5))
+ W6C = time.strftime("%H:%M", time.gmtime(secTimeC6))
 
 
- #https://stackoverflow.com/questions/775049/how-do-i-convert-seconds-to-hours-minutes-and-seconds
- #conert seconds to hours and minutes
 
- W3C = str(timedelta(minutes=minTimeC3))
- W4C = str(timedelta(minutes=minTimeC4))
- W5C = str(timedelta(minutes=minTimeC5))
- W6C = str(timedelta(minutes=minTimeC6))
-
-
- W3C1 = W3C[:-3] 
- W4C1 = W4C[:-3] 
- W5C1 = W5C[:-3] 
- W6C1 = W6C[:-3] 
-
-
- print("If you going to bed at",t2,"you should wake up at one of these times: ")
- print("4h and 30 min of sleep:", W3C1)
- print("6h of sleep:", W4C1)
- print("7h and 30 min of sleep:", W5C1)
- print("9h of sleep:", W6C1)
+ print("If you going to bed now, you should wake up at one of these times: ")
+ print("4h and 30 min of sleep:", W3C)
+ print("6h of sleep:", W4C)
+ print("7h and 30 min of sleep:", W5C)
+ print("9h of sleep:", W6C)
 
  
  print("How many hours are you to sleep?")
  sleepW.sleep = input()
  
- return  W3C1, W4C1, W5C1, W6C1
+ return  W3C, W4C, W5C, W6C
 
-#sleep = sleepW.sleep
+sleep = sleepW.sleep
 sleepW()
 
 
@@ -76,37 +68,38 @@ def sleepB():
 
 
  minTimeC3 = ( 15 + ((H * 60) + M) - (3 * cycle)) #4.5h of sleep
+ secTimeC3 = 60 * minTimeC3
+
  minTimeC4 = ( 15 + ((H * 60) + M) - (4 * cycle)) #6h of sleep
+ secTimeC4 = 60 * minTimeC4
+
  minTimeC5 = ( 15 + ((H * 60) + M) - (5 * cycle)) #7.5h of sleep
+ secTimeC5 = 60 * minTimeC5
+
  minTimeC6 = ( 15 + ((H * 60) + M) - (6 * cycle)) #9h of sleep
+ secTimeC6 = 60 * minTimeC6
 
- W3C = timedelta(minutes=minTimeC3)
- W4C = str(timedelta(minutes=minTimeC4))
- W5C = str(timedelta(minutes=minTimeC5))
- W6C = str(timedelta(minutes=minTimeC6))
-
-
- #W3C1 = W3C[:-3] 
- #W4C1 = W4C[:-3] 
- #W5C1 = W5C[:-3] 
- #W6C1 = W6C[:-3] 
-
- print(W3C.hour, W3C.minute)
+ W3C = time.strftime("%H:%M", time.gmtime(secTimeC3))
+ W4C = time.strftime("%H:%M", time.gmtime(secTimeC4))
+ W5C = time.strftime("%H:%M", time.gmtime(secTimeC5))
+ W6C = time.strftime("%H:%M", time.gmtime(secTimeC6))
 
 
- #print("If you wanto to wake up at ",t," you should go to bed at one of these times: ")
- #print("4h and 30 min of sleep:", W3C1)
- #print("6h of sleep:", W4C1)
- #print("7h and 30 min of sleep:", W5C1)
- #print("9h of sleep:", W6C1)
+
+ print("If you wanto to wake up at ", t," you should go to bed at one of these times: ")
+ print("4h and 30 min of sleep:", W3C)
+ print("6h of sleep:", W4C)
+ print("7h and 30 min of sleep:", W5C)
+ print("9h of sleep:", W6C)
 
  print("How many hours are you to sleep?")
  sleepB.sleep = input()
 
  return  W3C, W4C, W5C, W6C
 
-#sleep = sleepB.sleep 
+sleep = sleepB.sleep 
 sleepB()
+
 
 
 
