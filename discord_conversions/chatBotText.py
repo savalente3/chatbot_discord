e534522e7f2b08c15d9765e3c4ef29672e88ff25
gregorio alfonzo <alfonzoj@coventry.ac.uk>
2018-11-30 04:05:16 +0000

    Final improvements have been implemented

--- discord_conversions/chatBotText.py
+++ discord_conversions/chatBotText.py
@@ -1,10 +1,12 @@
+# Import of discord's API
 import discord
 from discord.ext import commands
 import asyncio
+# Import of files containing LOL API and database connection code 
 import game
 import database_con
 
-menu_options = ["1 - Check LOL stats", "2 - Do Sleep analysis", "3 - test"]
+menu_options = ["1 - Update your data", "2 - Check LOL stats", "3 - Do Sleep analysis", "3 - Check"]
 regions_list = game.regions_list
 bot = commands.Bot(command_prefix = '#')
 
@@ -18,10 +20,104 @@ async def on_ready():
 async def on_message(message):
     user_id = message.author.id
     if "hello" in message.content:
+        #here a check is done in the database to see if the user already exists
         database_con.db_search(user_id)
         if database_con.db_search.existing_user == True:
             user_name = str(database_con.db_search.name)
             await bot.send_message(message.channel, "Hello " + user_name + ". What can I do for you today ?")
+            menu_display = discord.Embed(
+                title = "Menu",
+                description = """Choose an option by writing it's corresponding number"""
+            )
+
+            for count, option in enumerate(menu_options):
+                menu_display.add_field(name ="Option{}".format(count+1), value = option, inline=False)
+
+            await bot.send_message(message.channel, embed = menu_display)
+            option_msg = await bot.wait_for_message(author = message.author)
+            menu_option = option_msg.content
+            menu_option = int(menu_option)
+
+            if menu_option == 1:
+                print("You chose to update your data")
+                await bot.send_message(message.channel, "What is your new name?")
+                user_name_msg = await bot.wait_for_message(author = message.author)
+                user_name = user_name_msg.content
+                await bot.send_message(message.channel, user_name + ", let's update your information")
+                await bot.send_message(message.channel, "How old are you now?")
+                user_age_msg = await bot.wait_for_message(author = message.author)
+                user_age = user_age_msg.content
+                print(user_age)
+                await bot.send_message(message.channel, "What is your height in centimeters?")
+                user_height_msg = await bot.wait_for_message(author = message.author)
+                user_height = user_height_msg.content
+                print(user_height)
+                await bot.send_message(message.channel, """Lastly I need your weight, no need to have shame, I'm a bot, I won't judge... """)
+                user_weight_msg = await bot.wait_for_message(author = message.author)
+                user_weight = user_weight_msg.content
+                print(user_weight)
+                # user's new values are inserted into database
+                database_con.db_update(user_id, user_name, user_age, user_height, user_weight)
+                await bot.send_message(message.channel, "Your data has been updated")
+
+            if menu_option == 2:
+                print("You chose option 2")
+                await bot.send_message(message.channel, "Please give me your summoner name.")
+                summoner_name_msg = await bot.wait_for_message(author = message.author)
+                summoner_name = summoner_name_msg.content
+                print(summoner_name)
+                await bot.send_message(message.channel, "Can you tell me your region?")
+                regions_display = discord.Embed(
+                    title = "Regions",
+                    description = "Choose one of the regions listed bellow:"
+                )
+
+                for count, region in enumerate(regions_list):
+                    regions_display.add_field(name= "Region {}".format(count+1), value = region, inline = False)
+                
+                await bot.send_message(message.channel, embed = regions_display)
+                region = await bot.wait_for_message(author = message.author)
+
+                if region:
+                    if region.content not in regions_list:
+                        await bot.send_message(message.channel, "That is not a valid region.")
+                        await bot.send_message(message.channel, embed = regions_display)
+                        region = await bot.wait_for_message(author = message.author)
+
+                    for i in range(len(regions_list)):
+                        if region.content == regions_list[i]:
+                            region_name = region.content
+                            print(region_name)
+                            await bot.send_message(message.channel, """Thank you for inserting your region's value""")
+                        i += 1
+                
+                game.summonerbyname(summoner_name, region_name)
+                summoner_ID = game.summonerbyname.summoner_ID
+                account_ID = game.summonerbyname.account_ID
+                summoner_ID = str(summoner_ID)
+                account_ID = str(account_ID)
+                game.mastery1(summoner_ID, region_name)
+                game.mastery2(summoner_ID, region_name)
+                game.matchList(account_ID, region_name)
+                mastery2 = game.mastery2.result
+                matchList_result = game.matchList.total_games
+                await bot.send_message(message.channel, "Here are your LOL stats:")
+                await bot.send_message(message.channel, "Your summoner ID is " + summoner_ID)
+                await bot.send_message(message.channel, "Your account ID is " + account_ID)
+                # here in the future it will print the table from mastery 1 function in image format
+                await bot.send_message(message.channel, mastery2)
+                # here in the future it will print the table from matchList function in image format
+                await bot.send_message(message.channel, matchList_result )
+                
+                print("end of option 2")
+                
+
+            elif menu_option == 3:
+                print("You have chosen option 3")
+
+            elif menu_option == 4:
+                print("You have chosen option 4")
+        
 
         else:
             await bot.send_message(message.channel, """Hello, my name is Chappie! It seems like it's the first time we meet... What is your name?""")
@@ -41,6 +137,9 @@ async def on_message(message):
             user_weight = user_weight_msg.content
             print(user_weight)
             await bot.send_message(message.channel, "Alright, I think I have everything I need! Thank you!")
+            # user's data get's inserted into database
+            database_con.db_insert(user_id, user_name, user_age, user_height, user_weight)
+
             menu_display = discord.Embed(
                 title = "Menu",
                 description = """Choose an option by writing it's corresponding number"""
@@ -56,7 +155,28 @@ async def on_message(message):
             menu_option = int(menu_option)
 
             if menu_option == 1:
-                print("You chose option 1")
+                print("You chose to update your data")
+                await bot.send_message(message.channel, "What is your new name?")
+                user_name_msg = await bot.wait_for_message(author = message.author)
+                user_name = user_name_msg.content
+                await bot.send_message(message.channel, user_name + ", let's update your information")
+                await bot.send_message(message.channel, "How old are you now?")
+                user_age_msg = await bot.wait_for_message(author = message.author)
+                user_age = user_age_msg.content
+                print(user_age)
+                await bot.send_message(message.channel, "What is your height in centimeters?")
+                user_height_msg = await bot.wait_for_message(author = message.author)
+                user_height = user_height_msg.content
+                print(user_height)
+                await bot.send_message(message.channel, """Lastly I need your weight, no need to have shame, I'm a bot, I won't judge... """)
+                user_weight_msg = await bot.wait_for_message(author = message.author)
+                user_weight = user_weight_msg.content
+                print(user_weight)
+                database_con.db_update(user_id, user_name, user_age, user_height, user_weight)
+                await bot.send_message(message.channel, "Your data has been updated")
+
+            if menu_option == 2:
+                print("You chose option 2")
                 await bot.send_message(message.channel, "Please give me your summoner name.")
                 summoner_name_msg = await bot.wait_for_message(author = message.author)
                 summoner_name = summoner_name_msg.content
@@ -107,15 +227,14 @@ async def on_message(message):
                 
 
 
-                print("end of option 1")
+                print("end of option 2")
                 
-
-            elif menu_option == 2:
-                print("You have chosen option 2")
 
             elif menu_option == 3:
                 print("You have chosen option 3")
 
-            database_con.db_insert(user_id, user_name, user_age, user_height, user_weight)
+            elif menu_option == 4:
+                print("You have chosen option 4")
+
 
 bot.run("NTA2OTgzMDY1NTUwMzIzNzIy.DsEzyg.WzxYWzRFJxxxnuABTNU8Vo8tmLk")
--- discord_conversions/database_con.py
+++ discord_conversions/database_con.py
@@ -1,5 +1,6 @@
-# Update your python to any version 3.6.x 
-# Do pip3 install PyMySQL
+# Update your python to any version 3.6.x for it to work with pymysql correctly
+# pymysql is a module that allows the creation of connection to mysql databases
+# pymysql's documentation can be found on https://pymysql.readthedocs.io/en/latest/
 
 import pymysql 
 
@@ -35,7 +36,31 @@ def db_insert(user_id, name, age, height, weight):
     con.close()
     return print("Database version: %s " % db_version)
 
-        
+
+def db_update(user_id, name, age, height, weight):
+
+    con = pymysql.connect("ricky.heliohost.org", "mrbean_admin", "admin123", "mrbean_chatbot")
+    cursor = con.cursor()
+
+    cursor.execute("SELECT VERSION()")
+    db_version = cursor.fetchone()
+
+    # sql_insert inserts data in the database if the user is having a conversation with the bot for the first time
+
+    sql_insert = "Update user SET name=%s, weight=%s, height=%s, age=%s WHERE user_id=%s"
+
+    try:
+        user_data = (name, weight, height, age, user_id)
+        cursor.execute(sql_insert, user_data)
+        con.commit
+
+    except IOError as e:
+        print(e)
+        con.rollback
+
+    con.close()
+    return print("Database version: %s " % db_version)
+
     
 def db_search(user_id):
     con = pymysql.connect("ricky.heliohost.org", "mrbean_admin", "admin123", "mrbean_chatbot")
